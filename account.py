baa9f539e67dabadea963cecee0b5b06493002f2
spiltcoffee21 <140124277+spiltcoffee21@users.noreply.github.com>
2024-06-14 00:40:04 +0000

    dfgcfgsdfgs

--- account.py
+++ account.py
@@ -4,4 +4,45 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+class Acc():
+    def __init__(self, name, acc_number, balance):
+        self.set_name(name)
+        self.set_acc_number(acc_number)
+        self.set_balance(balance)
+    
+    
+    def get_name(self):
+        return self._name
 
+         
+    def set_name(self, name):
+        if name == "":
+            raise ValueError("name the name")
+        self._name = name
+
+    
+    def get_acc_number(self):
+        return self._acc_number
+    
+    def set_acc_number(self, acc_number):
+        if isinstance(acc_number,(int,float)) and acc_number >= 0:
+            self._acc_number = acc_number
+        else:
+            raise ValueError("account number can not be negative")
+        
+        
+    def get_balance(self):
+        return self._balance
+        
+    def set_balance(self, balance):
+        if isinstance(balance,(int,float)):
+            self._balance = balance
+            
+    def deposit(self, balance):
+    
+    
+    
+    def withdraw(self, balance):
+        
+
+             
